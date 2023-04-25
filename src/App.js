c93c138d0295840448edc3f60da3a3ae4c1ed580
vishwajitvm <vishwajitmall50@gmail.com>
2023-04-25 18:53:19 +0530

    formate document with prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import HomeContainer from './Containers/HomeContainer';
+import logo from "./logo.svg";
+import "./App.css";
+import HomeContainer from "./Containers/HomeContainer";
 
 function App() {
   return (
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div>
       <div className="add-to-cart">
-      <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
       </div>
       <h1>Home Component</h1>
       <div className="cart-wrapper">
@@ -12,17 +12,13 @@ export default function Home() {
           <img src="https://freepngimg.com/thumb/apple_iphone/5-2-apple-iphone-png-picture.png" />
         </div>
         <div className="text-wrapper item">
-                    <span>
-                        I-Phone
-                    </span>
-                    <span>
-                        Price: $1000.00
-                    </span>
+          <span>I-Phone</span>
+          <span>Price: $1000.00</span>
         </div>
         <div className="btn-wrapper item">
-                    <button > Add To Cart</button>
+          <button> Add To Cart</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
