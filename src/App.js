c0dcd65b95b93093d5932c5bbfb83b3208cd1bce
vishwajitvm <vishwajitmall50@gmail.com>
2023-04-25 18:52:05 +0530

    design cart page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Components/Home';
+import HomeContainer from './Containers/HomeContainer';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <h1>hello world</h1>
-      </header> */}
-
-      <h1>App Component</h1>
       {/* <ParentComponent /> */}
-      <Home />
+      <HomeContainer />
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,26 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      dfgdf
+      <div className="add-to-cart">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+      </div>
+      <h1>Home Component</h1>
+      <div className="cart-wrapper">
+        <div className="image-wrapper item">
+          <img src="https://freepngimg.com/thumb/apple_iphone/5-2-apple-iphone-png-picture.png" />
+        </div>
+        <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+        </div>
+        <div className="btn-wrapper item">
+                    <button > Add To Cart</button>
+        </div>
+      </div>
     </div>
   )
 }
